5253895d869422f88ba19cadfd51ab2b75fe6c0d
Yanal Almir <yanalalmir@Yanals-Mac-Pro.local>
2022-01-25 10:28:00 +0100

    added routing and pages

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from "react";
 // tools
 import { Helmet } from 'react-helmet'
 import { ThemeProvider } from "styled-components";
+import { Route, Routes } from "react-router";
 // styles
 import { GlobalStyles } from "./styles/globalStyles";
 import { lightTheme, darkTheme } from './styles/theme'
+// pages
+import { HomePage, EpisodePage } from './pages'
 
 function App() {
   const currentTheme = 'light' ? lightTheme : darkTheme
@@ -13,12 +16,15 @@ function App() {
       <ThemeProvider theme={currentTheme}>
         <GlobalStyles />
           <Helmet>
-              <title>The Powerpuff Girls</title>
-              <link rel="preconnect" href="https://fonts.googleapis.com" />
-              <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-              <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
+            <title>The Powerpuff Girls</title>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
           </Helmet>
-          this is my app
+          <Routes>
+            <Route exact path='/' element={<HomePage />} />
+            <Route path='/season/:season/episode/:number' element={<EpisodePage />} />
+          </Routes>
       </ThemeProvider>
     </>
   );
---
+++ src/pages/EpisodePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EpisodePage = () => {
+  return (
+    <>
+        this is my episdoe page
+    </>
+  )
+};
+
+export default EpisodePage;
